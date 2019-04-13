6736e6c9acf6193f506656d69d098ba3b536a85f
kozak <vkozak@stream.ru>
2019-04-13 17:31:15 +0300

    markup for add link modal

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -2,18 +2,22 @@ import React, {Component} from 'react';
 import './button.less';
 
 type Props = {
+    process?: boolean,
+    color: 'white' | 'blue',
+    size: 'md' | 'lg',
     onClick: () => void
 }
 
 class Button extends Component<Props> {
     constructor(props: Props) {
-        super(props)
+        super(props);
     }
 
     render() {
         return (
-            <button className="button" onClick={this.props.onClick}>
-                {this.props.children}
+            <button className={`button ${this.props.color} ${this.props.size} ${this.props.process && 'process'}`}
+                    onClick={this.props.onClick}>
+                {this.props.process || this.props.children}
             </button>
         );
     }
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,60 +1,26 @@
 import React, {Component} from 'react';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import './header.less';
 import Logo from "../logo/logo";
 import ProfileMenu from "../profile-menu/profile-menu";
-import Button from "../button/button";
-import Modal from "../modal/modal";
+import AddNote from "../add-note/add-note";
 
-type Props = {}
-
-type State = {
-    showModal: boolean
-}
-
-class Header extends Component<Props, State> {
-
-    constructor(props: Props) {
-        super(props);
-        this.state = {
-            showModal: false
-        };
-    }
-
-    public showModalHandler = (): void => {
-        this.setState({
-            showModal: true
-        });
-    };
-
-    public closeModalHandler = (): void => {
-        this.setState({
-            showModal: false
-        });
-    };
-
+class Header extends Component {
     render() {
         return (
             <div className="header">
                 <div className="container">
-                    <div className="grid-noGutter-spaceBetween">
-                        <div className="col-4 grid">
+                    <div className="header-wrapper">
+                        <div className="header-left">
                             <Logo/>
                             <span className="title desktop-title">GLStorage</span>
                             <span className="title tablet-title">GLS</span>
                         </div>
-                        <div className="col-8 grid-right">
-                            <Button onClick={this.showModalHandler}>
-                                <FontAwesomeIcon icon="plus"/>
-                                <span className="add-link-button-text">Добавить</span>
-                            </Button>
+                        <div className="header-right">
+                            <AddNote/>
                             <ProfileMenu/>
                         </div>
                     </div>
                 </div>
-                <Modal open={this.state.showModal} close={this.closeModalHandler}>
-                    <div>ghbdnt</div>
-                </Modal>
             </div>
         );
     }
